c92fdaf6d8b1e501628d3e9c49b65508d7489c52
Will Dudley <14932240+WillDudley@users.noreply.github.com>
2019-02-11 01:20:58 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="margingame",
+    version="0.0.1",
+    author="WillDudley",
+    author_email="Will2346@live.co.uk",
+    description="A package for the margin game",
+    long_description=long_description,
+    long_description_content_type="text/reStructuredText",
+    url="https://github.com/WillDudley/margingame",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
