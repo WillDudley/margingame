b879bb9a96d5a276b87fde5bfdb63539abd9acdc
Will Dudley <14932240+WillDudley@users.noreply.github.com>
2019-02-17 20:25:41 +0000

    V0.2.4
    Logit not added, instead changed calculation of probabilities

--- margingame/payoff/probabilities.py
+++ margingame/payoff/probabilities.py
@@ -2,19 +2,22 @@ import numpy as np
 from scipy.stats import norm
 
 
-def gaussian_probabilities(delta, prob_b, variance=1):
+def gaussian_probabilities(delta, prob_b_without_margin, variance=1):
     """
     :param delta: Radius of uncertainty margin.
-    :param prob_b: Expected probability of attack being successful (breaching) given that there is no uncertainty
-                        margin. In other words, the mean target of the attack.
+    :param prob_b_without_margin: Expected probability of attack being successful (breaching) given that there is no
+                                    uncertainty margin. In other words, the mean target of the attack.
     :param variance: Variance in the result of the attack.
     :returns: Numpy array containing the probability of attack failing, being detained and succeeding, respectively.
     """
-    d = norm.ppf(1 - prob_b, scale=variance)  # Finds where the margin is around
 
-    prob_q = norm.cdf(-delta, loc=-d, scale=variance)  # Gives area to left of left side of margin
-    prob_less_than_delta = norm.cdf(delta, loc=-d, scale=variance)
-    prob_b = 1 - prob_less_than_delta  # Gives area to right side of margin
-    prob_d = prob_less_than_delta - prob_q  # Gives area in margin
+    midpoint = norm.ppf(1 - prob_b_without_margin, scale=variance)  # Finds the midpoint of the margin.
+
+    prob_q = norm.cdf(midpoint - delta, scale=variance)  # Gives area to the left of the left side of the margin.
+
+    prob_b = 1 - norm.cdf(midpoint + delta, scale=variance)  # Gives area to the right of the right side
+    # of the margin.
+
+    prob_d = 1 - prob_b - prob_q  # Gives area in the margin
 
     return np.array([prob_q, prob_d, prob_b])
